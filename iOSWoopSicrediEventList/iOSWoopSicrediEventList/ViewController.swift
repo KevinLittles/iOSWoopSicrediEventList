c59e48155f6a862f60b80317127aec4703e31c24
ProKevinRibeiro <49958388+ProKevinRibeiro@users.noreply.github.com>
2021-08-16 20:29:26 -0300

    Xcode project created

---
+++ iOSWoopSicrediEventList/iOSWoopSicrediEventList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSWoopSicrediEventList
+//
+//  Created by Kevin Ribeiro on 16/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
