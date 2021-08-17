9900ab1b47286da5258a40a2c8967cf8980e0e36
ProKevinRibeiro <49958388+ProKevinRibeiro@users.noreply.github.com>
2021-08-16 23:02:10 -0300

    Cocoapods init, RxSwift installed

---
+++ iOSWoopSicrediEventList/Pods/Target Support Files/Pods-iOSWoopSicrediEventList-iOSWoopSicrediEventListUITests/Pods-iOSWoopSicrediEventList-iOSWoopSicrediEventListUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSWoopSicrediEventList_iOSWoopSicrediEventListUITests : NSObject
+@end
+@implementation PodsDummy_Pods_iOSWoopSicrediEventList_iOSWoopSicrediEventListUITests
+@end
---
+++ iOSWoopSicrediEventList/Pods/Target Support Files/Pods-iOSWoopSicrediEventListTests/Pods-iOSWoopSicrediEventListTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSWoopSicrediEventListTests : NSObject
+@end
+@implementation PodsDummy_Pods_iOSWoopSicrediEventListTests
+@end
