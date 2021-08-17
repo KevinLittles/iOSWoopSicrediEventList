3f52d48f141f19d8a4f9464b801265f5570b0881
ProKevinRibeiro <49958388+ProKevinRibeiro@users.noreply.github.com>
2021-08-17 13:01:05 -0300

    Checkin Model done

--- iOSWoopSicrediEventList/iOSWoopSicrediEventList/Model/CheckinModel.swift
+++ iOSWoopSicrediEventList/iOSWoopSicrediEventList/Model/CheckinModel.swift
@@ -4,3 +4,15 @@
 //
 //  Created by Kevin Ribeiro on 17/08/21.
 //
+
+import Foundation
+
+struct CheckinModel: Codable {
+    let eventID, name, email: String
+
+    enum CodingKeys: String, CodingKey {
+        case eventID = "eventId"
+        case name, email
+    }
+}
+
